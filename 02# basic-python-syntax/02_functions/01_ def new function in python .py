4097bd9e4a2d67b2e526b86b8353646db90639a6
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-12-11 20:16:19 +0200

    DH-12 New function about area triangle

---
+++ 02# basic-python-syntax/02_functions/01_ def new function in python .py
@@ -0,0 +1,7 @@
+# greeting function in python
+def area_triangle(base, height):
+    return base * height / 2
+    area_a = area_triangle(3, 4)
+    area_b = area_triangle(2, 5)
+    Tota = (area_b + area_b)
+    print("The sum of the both area is " + str(area_a))
