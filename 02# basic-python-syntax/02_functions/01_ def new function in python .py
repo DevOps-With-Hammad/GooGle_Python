f35e3d8bec7df5b06faa2f5ef5e0296b3f186333
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-12-11 20:38:44 +0200

    DH-12 seconds to hours converter

--- 02# basic-python-syntax/02_functions/01_ def new function in python .py
+++ 02# basic-python-syntax/02_functions/01_ def new function in python .py
@@ -1,7 +1,15 @@
-# greeting function in python
-def area_triangle(base, height):
-    return base * height / 2
-    area_a = area_triangle(3, 4)
-    area_b = area_triangle(2, 5)
-    Tota = (area_b + area_b)
-    print("The sum of the both area is " + str(area_a))
+# Seconds to hours converter.
+
+def second_to_hours(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+
+
+total = input("please enter your converted value ")
+hours, minutes, remaining_seconds =second_to_hours(int(total))
+print(hours, minutes, remaining_seconds)
+
+for second_to_hours in range (0, 10):
+    print(hours, minutes, remaining_seconds)
